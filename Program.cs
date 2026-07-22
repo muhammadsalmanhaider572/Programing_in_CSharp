26cc3ed19f1e0a92976ce56fc4bb7beb6b0f3833
MuhammadSalmanHaider <msalmanhaider572@gmail.com>
2026-07-22 09:29:25 +0500

    Console_Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace OOP_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog { Name = "Buddy", Bread = "Golden Retriever" };
+            myDog.MakeSound();
+        }
+        class Animal
+        {
+            public string Name { get; set; }
+            public virtual void MakeSound() => Console.WriteLine("Some animals makes generic sounds");
+        }
+        class Dog : Animal
+        {
+            public string Bread { get; set; }
+            public override void MakeSound() => Console.WriteLine($"{Name} the {Bread} says: Woof! Woof!");
+        }
+    }
+}
+
